002f1a8eaacca06ffcb0704864f81c1f4ed1af67
suavmoy samanta <suvamoysamanta907@gmail.com>
2026-01-17 00:29:51 +0530

    DataTable for cryptro Graph

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -15,7 +15,7 @@ function Header() {
                   <div className="main-container inner">
                           <div className="flex gap-4">
 <Link href="/">
-        <Image src="logo.svg" alt="marketplace" width={70} height={20} className='rounded-3xl shadow-2xs border-2 border-none '/>
+        <Image src="logo.svg" alt="marketplace" width={55} height={25} className='rounded-3xl shadow-2xs border-2 border-none '/>
                         </Link> 
                                 <div className="mt-2  -webkit-font-smoothing: antialiased, font-style: italic text-1xl  font-semibold  shadow-4xl">
                                      MarKetLedGer
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,41 @@
 import React from 'react'
+import Image from "next/image"
+import DataTabel from './components/DataTabel'
+
+
+
 
 const page = () => {
-  return (
-    <div>
+  return <main className='main-container'>
+    <section className='home-grid'>
+
+      <div id="coin-overview">
+<div className='header'>
+       <Image src="https://assets.coingecko.com/coins/images/1/large/bitcoin.png" alt="Bitcoin" width={30} height={20}/>
+       <div className="info">
+      <p>BitCoin / BTC</p>
+      <h1>
+         $8976373.00
+      </h1>
 
     </div>
+    </div>  
+        
+      </div>
 
-  )
+
+      <p>Treading Coins</p>
+
+      <DataTabel/>
+    
+    </section>
+
+    <section className='w-full mt-7 space-y-4'>
+      <p>
+        Categories
+      </p>
+
+    </section>
+  </main>
 }
 export default page
